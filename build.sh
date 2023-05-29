84202537108e027e10d8523d080f1cdbae6bc43c
Dave Templin <dtempx@gmail.com>
2023-05-28 20:32:25 -0700

    refactor dist packaging

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
 npx tsc
+
+# ESM BUILD
 npx tsc -p tsconfig.esm.json
 echo '{"type":"module"}' > dist/esm/package.json
+node build-tools/version-stamp dist/esm/extract/index.js
+
+# COMMONJS BUILD
 npx tsc -p tsconfig.cjs.json
 echo '{"type":"commonjs"}' > dist/cjs/package.json
+node build-tools/version-stamp dist/cjs/extract/index.js
+
+# UMD BUILD
 npx rollup index.js --format umd --name syphonx --file dist/umd/syphonx.js
-npx rollup index.js --format iife --name syphonx --file dist/iife/syphonx.js
+node build-tools/version-stamp dist/umd/syphonx.js
+
+# IIFE BUILD
+npx rollup extract/index.js --format iife --name syphonx --file dist/iife/syphonx.js
 node build-tools/version-stamp dist/iife/syphonx.js
+
+# JQUERY BUILD
 node build-tools/apply-template \
     build-tools/templates/syphonx-jquery.txt \
     node_modules/jquery/dist/jquery.slim.min.js \
