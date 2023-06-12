0b1b38b5aa39fc55f0c4271f17ee59697514dc11
Dave Templin <dtempx@gmail.com>
2023-06-12 14:07:22 -0700

    revert build changes

--- build.sh
+++ build.sh
@@ -21,18 +21,12 @@ node build-tools/apply-template \
 
 # ESM BUILD
 npx tsc -p tsconfig.esm.json
-rm -rf dist/esm/extract/private # purge empty declaration files that only contained interface definitions
-rm dist/esm/extract/public/*.js*
-rm dist/esm/template.js*
 echo '{"type":"module"}' > dist/esm/package.json
 node build-tools/version-stamp dist/esm/extract/context.js
 node build-tools/embed-script dist/esm/execute.js dist/iife/syphonx-jquery.min.js -o dist/esm/execute.js
 
 # COMMONJS BUILD
 npx tsc -p tsconfig.cjs.json
-rm -rf dist/cjs/extract/private # purge empty declaration files that only contained interface definitions
-rm dist/cjs/extract/public/*.js*
-rm dist/cjs/template.js*
 echo '{"type":"commonjs"}' > dist/cjs/package.json
 node build-tools/version-stamp dist/cjs/extract/context.js
 node build-tools/embed-script dist/cjs/execute.js dist/iife/syphonx-jquery.min.js -o dist/cjs/execute.js
