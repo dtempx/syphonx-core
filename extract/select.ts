134cc2211b1593f84f96061a78e6fb7cd3e54d30
Dave Templin <dave.templin@pricespider.com>
2024-01-22 11:37:19 -0800

    tweak select interface

--- extract/select.ts
+++ extract/select.ts
@@ -1,6 +1,7 @@
 import { Controller } from "./controller.js";
 import { Action, ExtractState, Select } from "./public/index.js";
 import { flattenTemplateSelect } from "./utilities.js";
+import { Template } from "../template.js";
 
 export interface SelectOptions {
     url?: string;
@@ -10,10 +11,10 @@ export interface SelectOptions {
     root?: unknown;
 }
 
-export function select(selects: Select[], options: SelectOptions = {}): ExtractState {
+export function select(selects: Select[] | Template, options: SelectOptions = {}): ExtractState {
     if (!Array.isArray(selects) && typeof selects === "object" && selects !== null && (selects as {}).hasOwnProperty("actions"))
         selects = flattenTemplateSelect((selects as { actions: Action[] }).actions);
     const controller = new Controller(options);
-    const data = controller.select(selects);
+    const data = controller.select(selects as Select[]);
     return { ...controller.state, data };
 }
